ce34a9e26808a84568edb36c33cd2d82a4f2d5aa
Franz <Franz@DESKTOP-MICEU0B>
2020-02-13 06:57:01 +0800

    AutoLogout

--- SMFGC/handleClient.cs
+++ SMFGC/handleClient.cs
@@ -11,8 +11,10 @@ using MySql.Data.MySqlClient;
 
 using static SMFGC.Program;
 
-namespace SMFGC {
-    class handleClient {
+namespace SMFGC
+{
+    class handleClient
+    {
         TcpClient clientSocket;
         readonly MySqlConnection conn = new MySqlConnection(pVariables.sConn);
         MySqlCommand cmd;
@@ -20,17 +22,20 @@ namespace SMFGC {
 
         Thread t;
 
-        public void startClient(TcpClient inClientSocket) {
+        public void startClient(TcpClient inClientSocket)
+        {
             this.clientSocket = inClientSocket;
             t = new Thread(doTasks);
             t.Start();
         }
 
-        public void exitThread() {
+        public void exitThread()
+        {
             t.Abort();
         }
 
-        private void doTasks() {
+        private void doTasks()
+        {
             // Buffer for reading data
             Byte[] buffer = new Byte[1024];
             String dev_ip = "", room_name = "", data = null, faculty = "";
@@ -40,23 +45,27 @@ namespace SMFGC {
 
             DateTime end_time = DateTime.Parse("00:00:00");
 
-            try {
+            try
+            {
                 NetworkStream stream = clientSocket.GetStream();
 
                 int i;
-                while ((i = stream.Read(buffer, 0, buffer.Length)) != 0) {
+                while ((i = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
 
                     // Translate data bytes to a ASCII string.
                     data = System.Text.Encoding.ASCII.GetString(buffer, 0, i);
 
-                    if (!data.Contains("DEV:")) {
+                    if (!data.Contains("DEV:"))
+                    {
                         Console.WriteLine("Unknown Device: \"{0}\" (Server Connection Closed)", data);
                         sysLog(0, "", "devinfo", "Unknown: (Sending data that doesn't known to server!) Device Denied.", 16);
                         break;
                     }
 
                     // Verify if the device is known to server.
-                    if (!dev_verified) {
+                    if (!dev_verified)
+                    {
                         dev_id = Convert.ToInt32(data.Split(',')[0].Replace("DEV:", ""));
                         dev_ip = ((IPEndPoint)clientSocket.Client.RemoteEndPoint).Address.ToString();
 
@@ -67,7 +76,8 @@ namespace SMFGC {
                         conn.Open();
                         reader = cmd.ExecuteReader();
 
-                        if (reader.Read()) {
+                        if (reader.Read())
+                        {
                             room_id = Convert.ToInt32(reader["room_id"]);
                             room_name = reader["classroom"].ToString();
                             relay1 = Convert.ToBoolean((reader["relay1"]));
@@ -79,11 +89,13 @@ namespace SMFGC {
                         }
                         conn.Close();
 
-                        if (dev_verified) {
+                        if (dev_verified)
+                        {
                             // Update the status and uptime
                             RoomUpdateSingle(room_id, 2);
                         }
-                        else {
+                        else
+                        {
                             Console.WriteLine("Unknown Device: {0}, {1} (Server Connection Closed)", dev_id, dev_ip);
                             sysLog(dev_id, "", "devinfo", "Unknown: (Device is known but not registered to database!) Device Denied.", 16);
                             break;
@@ -91,41 +103,54 @@ namespace SMFGC {
                     }
 
                     // CONTINUES BLINKING RED 	- 10 MINUTES LEFT WARNING
-                    if (dev_verified && room_status == 3 && !data.Contains("UID:")) {
+                    if (dev_verified && room_status == 3 && !data.Contains("UID:"))
+                    {
                         TimeSpan tdiff = end_time - DateTime.UtcNow;
 
-                        if (tdiff.Minutes < 10 && timeleft_led <= 0) {
+                        if (tdiff.Minutes < 10)
+                        {
                             // Send command to blink LEDs
-                            if (tdiff.Minutes < 0) {
+                            if (tdiff.Minutes <= 0)
+                            {
                                 // Turn off all relays
                                 msg = System.Text.Encoding.ASCII.GetBytes("d");
                                 stream.Write(msg, 0, msg.Length);
 
+                                room_status = 2;
+                                RoomUpdateSingle(room_id, room_status);
+
                                 sysLog(dev_id, "", "userauth", "Room: " + room_name + ", Faulty: " + faculty + ", Shedule Ended.", 64);
                                 Console.WriteLine("Schedule ended on Room: {0} automatically.", room_name);
                             }
-                            else { 
-                                msg = System.Text.Encoding.ASCII.GetBytes("f");
-                                stream.Write(msg, 0, msg.Length);
+                            else
+                            {
+                                if (timeleft_led <= 0)
+                                {
+                                    msg = System.Text.Encoding.ASCII.GetBytes("f");
+                                    stream.Write(msg, 0, msg.Length);
 
-                                timeleft_led = 3; //reset
-                                Console.WriteLine("Schedule started on Room: {0} -> {1} Minutes and {2} Seconds Left.", room_name, tdiff.Minutes, tdiff.Seconds);
+                                    timeleft_led = 3; //reset
+                                    Console.WriteLine("Schedule on Room: {0} is ending in -> {1} Minutes and {2} Seconds Left.", room_name, tdiff.Minutes, tdiff.Seconds);
+                                }
+                                else
+                                {
+                                    timeleft_led -= 1;
+                                }
                             }
-                        }
-                        else {
-                            timeleft_led -= 1;
                         }
                     }
 
                     // Process the data sent by the client.
-                    if (dev_verified && data.Contains("UID:")) {
+                    if (dev_verified && data.Contains("UID:"))
+                    {
 
                         string uidtag = data.Split(',')[1].Split(' ')[1];
                         string tag_type = data.Split(',')[2];
 
                         bool valid_uidtag = checkUidTagToDB(uidtag);
 
-                        if (tag_type == "IN" && valid_uidtag) {
+                        if (tag_type == "IN" && valid_uidtag)
+                        {
 
                             // Tag is ok but check schedule
                             cmd = conn.CreateCommand();
@@ -134,22 +159,27 @@ namespace SMFGC {
                             conn.Open();
                             reader = cmd.ExecuteReader();
 
-                            if (reader.Read()) {
+                            if (reader.Read())
+                            {
 
                                 sched_id = Convert.ToInt32(reader["sched_id"].ToString());
                                 faculty = reader["faculty"].ToString();
                                 end_time = DateTime.Parse(reader["end_time"].ToString());
 
-                                if (relay1 && relay2) {
+                                if (relay1 && relay2)
+                                {
                                     msg = System.Text.Encoding.ASCII.GetBytes("c");
                                 }
-                                else if (relay1 && !relay2) {
+                                else if (relay1 && !relay2)
+                                {
                                     msg = System.Text.Encoding.ASCII.GetBytes("a");
                                 }
-                                else if (!relay1 && relay2) {
+                                else if (!relay1 && relay2)
+                                {
                                     msg = System.Text.Encoding.ASCII.GetBytes("b");
                                 }
-                                else {
+                                else
+                                {
                                     msg = System.Text.Encoding.ASCII.GetBytes("e");
                                 }
                                 // Send back a response.
@@ -161,7 +191,8 @@ namespace SMFGC {
                                 room_status = 3;
                                 RoomUpdateLastUID(room_id, uidtag, room_status);
                             }
-                            else {
+                            else
+                            {
                                 // Send command to blink LEDs
                                 msg = System.Text.Encoding.ASCII.GetBytes("f");
                                 stream.Write(msg, 0, msg.Length);
@@ -172,7 +203,8 @@ namespace SMFGC {
 
                             conn.Close();
                         }
-                        else if (tag_type == "OUT" && valid_uidtag) {
+                        else if (tag_type == "OUT" && valid_uidtag)
+                        {
 
                             // Check if LOGIN UID != LOGOUT UID
                             // getlast UID from Database
@@ -183,7 +215,8 @@ namespace SMFGC {
                             conn.Open();
                             reader = cmd.ExecuteReader();
 
-                            if (reader.Read()) {
+                            if (reader.Read())
+                            {
                                 // Turn off all relays
                                 msg = System.Text.Encoding.ASCII.GetBytes("d");
                                 stream.Write(msg, 0, msg.Length);
@@ -193,7 +226,8 @@ namespace SMFGC {
                                 Console.WriteLine("Schedule ended on Room: {0}:{1} with Schedule ID: {2}", room_id, room_name, sched_id);
                                 sysLog(dev_id, uidtag, "userauth", "Room: " + room_name + ", Faulty: " + faculty + ", Logged out.", 64);
                             }
-                            else {
+                            else
+                            {
                                 // Send command to blink LEDs
                                 msg = System.Text.Encoding.ASCII.GetBytes("g");
                                 stream.Write(msg, 0, msg.Length);
@@ -202,7 +236,8 @@ namespace SMFGC {
                             }
                             conn.Close();
                         }
-                        else {
+                        else
+                        {
                             // Send command to blink LEDs
                             msg = System.Text.Encoding.ASCII.GetBytes("x");
                             stream.Write(msg, 0, msg.Length);
@@ -211,13 +246,16 @@ namespace SMFGC {
                             sysLog(dev_id, uidtag, "userauth", "Unknown User [RFID Tag]: Denied by server.", 48);
                         }
                     }
-                    else if (dev_verified && data.Contains("PZM:")) {
+                    else if (dev_verified && data.Contains("PZM:"))
+                    {
 
-                        if (data.Contains("NaN")) {
+                        if (data.Contains("NaN"))
+                        {
                             sysLog(dev_id, "", "system", "Part Zone Expansion Module (PZEM) is not reading data.", 16);
                             Console.WriteLine("Error Reading PZEM Data.");
                         }
-                        else {
+                        else
+                        {
                             data = data.Split(',')[1].Replace("PZM:", "").Trim();
 
                             String[] pzem = data.Split('-');
@@ -238,13 +276,15 @@ namespace SMFGC {
                             Console.WriteLine("PZEM Data recorded to database.");
                         }
                     }
-                    else if (dev_verified && data.Contains("RLY:")) {
+                    else if (dev_verified && data.Contains("RLY:"))
+                    {
                         Console.WriteLine("Device acknowledge the command.");
                         sysLog(dev_id, "", "devinfo", "Client aknowledge the command given by the server.", 64);
                     }
 
                     // get ping status checked by pingClient class
-                    if (dev_verified && next_tcp_check <= 0) {
+                    if (dev_verified && next_tcp_check <= 0)
+                    {
 
                         cmd = conn.CreateCommand();
                         cmd.CommandText = pVariables.qTCPBrokenCheck;
@@ -252,10 +292,12 @@ namespace SMFGC {
                         conn.Open();
                         reader = cmd.ExecuteReader();
 
-                        if (reader.Read()) {
+                        if (reader.Read())
+                        {
                             Console.WriteLine("TCP Link check on Device: {0}, IP: {1} - OK.", dev_id, dev_ip);
                         }
-                        else {
+                        else
+                        {
                             RoomUpdateSingle(room_id, 1);
                             sysLog(dev_id, "", "devinfo", "Client connection lost.", 16);
                             Console.WriteLine("System detected Broken TCP Link on Device: {0}, IP: {1} - Connection Lost.", dev_id, dev_ip);
@@ -264,24 +306,28 @@ namespace SMFGC {
                         conn.Close();
                         next_tcp_check = 10;
                     }
-                    else if (dev_verified && next_tcp_check > 0) {
+                    else if (dev_verified && next_tcp_check > 0)
+                    {
                         next_tcp_check -= 1;
                     }
 
                     Thread.Sleep(1000);
                 }
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 Console.WriteLine("Error: " + ex.ToString());
             }
-            finally {
+            finally
+            {
                 // If we're in error (timeout, thread stopped...) close socket and return
 
                 if (conn.State == ConnectionState.Open) conn.Close();
 
                 if (dev_verified) RoomUpdateSingle(room_id, 1);
 
-                if (clientSocket != null) {
+                if (clientSocket != null)
+                {
                     if (clientSocket.Connected) clientSocket.Close();
                     Console.WriteLine("Client Connection Closed.");
                     sysLog(0, "", "system", "Client connection closed by the server.", 16);
@@ -289,7 +335,8 @@ namespace SMFGC {
             }
         }
 
-        private bool checkUidTagToDB(string tag) {
+        private bool checkUidTagToDB(string tag)
+        {
 
             if (conn.State == ConnectionState.Open) conn.Close();
 
@@ -307,7 +354,8 @@ namespace SMFGC {
             return ret;
         }
 
-        private void RoomUpdateSingle(int room_id, int status) {
+        private void RoomUpdateSingle(int room_id, int status)
+        {
             if (conn.State == ConnectionState.Open) conn.Close();
 
             // Update the status and uptime
@@ -320,7 +368,8 @@ namespace SMFGC {
             conn.Close();
         }
 
-        private void RoomUpdateLastUID(int room_id, string uidtag, int status) {
+        private void RoomUpdateLastUID(int room_id, string uidtag, int status)
+        {
             if (conn.State == ConnectionState.Open) conn.Close();
 
             // Update room last_uidtag
