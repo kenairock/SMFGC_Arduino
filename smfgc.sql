ba92d9dc079f484f1acdb5737c077fd5f9618ba8
Christian Galvez <galvezchristian16@gmail.com>
2020-02-15 21:17:05 +0800

    update

--- smfgc.sql
+++ smfgc.sql
@@ -50,7 +50,7 @@ CREATE TABLE `course_tb` (
   `year` int(4) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `Name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `course_tb` */
 
@@ -82,6 +82,8 @@ CREATE TABLE `device_tb` (
   `ip_addr` varchar(16) NOT NULL,
   `mac_addr` varchar(17) NOT NULL,
   `port` int(5) DEFAULT '2316',
+  `rfid` tinyint(1) DEFAULT '1',
+  `pzem` tinyint(1) DEFAULT '1',
   `status` tinyint(1) NOT NULL DEFAULT '0',
   `last_uidtag` varchar(12) DEFAULT NULL,
   `uptime` datetime DEFAULT NULL,
@@ -89,11 +91,11 @@ CREATE TABLE `device_tb` (
   UNIQUE KEY `IP Address` (`ip_addr`),
   UNIQUE KEY `MAC Address` (`mac_addr`),
   UNIQUE KEY `Serial Number` (`serial_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=517 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=519 DEFAULT CHARSET=utf8;
 
 /*Data for the table `device_tb` */
 
-insert  into `device_tb`(`id`,`serial_no`,`ip_addr`,`mac_addr`,`port`,`status`,`last_uidtag`,`uptime`) values (514,415051,'192.168.1.51','DE-AD-BE-EF-FE-E0',2316,0,NULL,NULL),(515,515052,'192.168.1.52','DE-AD-BE-EF-FE-E1',2316,0,NULL,NULL);
+insert  into `device_tb`(`id`,`serial_no`,`ip_addr`,`mac_addr`,`port`,`rfid`,`pzem`,`status`,`last_uidtag`,`uptime`) values (514,415051,'192.168.1.51','DE-AD-BE-EF-FE-E0',2316,1,1,0,NULL,NULL),(515,515052,'192.168.1.52','DE-AD-BE-EF-FE-E1',2316,1,1,0,NULL,NULL),(518,556895,'10.0.1.100','DE-AD-BE-EF-FE-E2',2316,1,1,1,NULL,NULL);
 
 /*Table structure for table `faculty_tb` */
 
@@ -101,19 +103,23 @@ DROP TABLE IF EXISTS `faculty_tb`;
 
 CREATE TABLE `faculty_tb` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `level` tinyint(1) NOT NULL DEFAULT '1',
   `uidtag` varchar(12) NOT NULL,
   `title` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `mi` varchar(45) NOT NULL,
   `picture` mediumblob,
+  `sfv_count` int(5) NOT NULL DEFAULT '0',
+  `sfv_time` time NOT NULL DEFAULT '01:00:00',
+  `sfv_limit` int(5) NOT NULL DEFAULT '10',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UIDTAG` (`uidtag`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 /*Data for the table `faculty_tb` */
 
-insert  into `faculty_tb`(`id`,`uidtag`,`title`,`last_name`,`first_name`,`mi`,`picture`) values (1,'e563c223','Doc.','Miguel','Jolly','A.',NULL);
+insert  into `faculty_tb`(`id`,`level`,`uidtag`,`title`,`last_name`,`first_name`,`mi`,`picture`,`sfv_count`,`sfv_time`,`sfv_limit`) values (1,1,'e563c223','Doc.','Miguel','Jolly','A.',NULL,1,'01:00:00',10);
 
 /*Table structure for table `pzem_tb` */
 
@@ -160,7 +166,7 @@ CREATE TABLE `schedule_tb` (
 
 /*Data for the table `schedule_tb` */
 
-insert  into `schedule_tb`(`id`,`course_id`,`subject_id`,`room_id`,`faculty_id`,`day`,`start_time`,`end_time`) values (11,1,10,5,1,'Monday','15:00:00','18:00:00');
+insert  into `schedule_tb`(`id`,`course_id`,`subject_id`,`room_id`,`faculty_id`,`day`,`start_time`,`end_time`) values (11,1,10,5,1,'Saturday','12:00:00','15:00:00');
 
 /*Table structure for table `subject_tb` */
 
@@ -184,15 +190,14 @@ DROP TABLE IF EXISTS `syslog_tb`;
 
 CREATE TABLE `syslog_tb` (
   `ts` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
-  `dev_id` int(8) NOT NULL,
   `process` varchar(25) NOT NULL,
   `alert` int(3) NOT NULL,
-  `message` text NOT NULL,
-  KEY `logsDevID` (`dev_id`),
-  CONSTRAINT `SL_Device` FOREIGN KEY (`dev_id`) REFERENCES `device_tb` (`id`) ON UPDATE CASCADE
+  `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `syslog_tb` */
+
+insert  into `syslog_tb`(`ts`,`process`,`alert`,`message`) values ('2020-02-14 22:57:15.812','sys',64,'Server started.'),('2020-02-14 23:00:21.242','sys',64,'Server started.'),('2020-02-14 23:30:16.585','sys',64,'Server started.'),('2020-02-15 15:11:29.649','sys',64,'Server started.'),('2020-02-15 15:12:02.105','sys',64,'Server closed.'),('2020-02-15 15:13:47.119','sys',64,'Server started.'),('2020-02-15 15:13:49.244','sys',64,'Server closed.'),('2020-02-15 15:13:54.076','sys',64,'Server started.'),('2020-02-15 15:14:47.159','sys',64,'Server started.'),('2020-02-15 15:14:49.481','sys',64,'Server closed.'),('2020-02-15 15:15:28.207','sys',64,'Server started.'),('2020-02-15 15:15:33.927','sys',64,'Server closed.'),('2020-02-15 15:19:27.640','sys',64,'Server started.'),('2020-02-15 15:20:12.713','sys',64,'Server started.'),('2020-02-15 15:24:46.338','sys',64,'Server started.'),('2020-02-15 15:26:20.982','sys',64,'Server started.'),('2020-02-15 15:26:45.061','sys',64,'Server started.'),('2020-02-15 15:27:11.475','sys',64,'Server started.'),('2020-02-15 15:29:34.685','sys',64,'Server started.'),('2020-02-15 15:30:01.436','sys',64,'Server started.'),('2020-02-15 15:31:18.924','sys',64,'Server started.'),('2020-02-15 15:32:57.683','sys',64,'Server started.'),('2020-02-15 15:34:20.246','sys',64,'Server started.'),('2020-02-15 15:34:49.270','sys',64,'Server started.'),('2020-02-15 15:35:13.169','sys',64,'Server started.'),('2020-02-15 15:35:43.684','sys',64,'Server started.'),('2020-02-15 17:55:03.283','sys',64,'Server started.'),('2020-02-15 18:00:59.351','sys',64,'Server started.'),('2020-02-15 18:01:49.455','sys',64,'Server started.'),('2020-02-15 18:02:15.189','sys',64,'Server started.'),('2020-02-15 18:04:02.194','sys',64,'Server started.'),('2020-02-15 18:05:05.733','sys',64,'Server started.'),('2020-02-15 18:06:00.487','sys',64,'Server started.');
 
 /*Table structure for table `user_tb` */
 
@@ -203,14 +208,15 @@ CREATE TABLE `user_tb` (
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `fullname` varchar(45) NOT NULL,
-  `dept_id` int(10) DEFAULT NULL,
+  `dept_id` int(10) DEFAULT '0',
   `role` varchar(8) NOT NULL,
-  `picture` mediumblob,
   PRIMARY KEY (`id`),
   UNIQUE KEY `Username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user_tb` */
+
+insert  into `user_tb`(`id`,`username`,`password`,`fullname`,`dept_id`,`role`) values (5,'francis','admin','Francis Galvez',0,'Admin'),(6,'james','123','James Bond',1,'Dean');
 
 /* Function  structure for function  `UC_Words` */
 
